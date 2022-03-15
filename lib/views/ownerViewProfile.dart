2ac34490fb6f83c15dd0864754eca5700f5ad01c
joehurdles <joeenocent@gmail.com>
2022-03-14 19:13:39 -0700

    Upload property linked

--- lib/views/ownerViewProfile.dart
+++ lib/views/ownerViewProfile.dart
@@ -3,7 +3,8 @@ import 'package:rent/services/api_manager.dart';
 import 'package:rent/views/card_view.dart';
 import 'package:rent/views/chat_view.dart';
 import 'package:rent/views/landlord_register.dart';
-import 'package:rent/views/houseGrid_view.dart';
+
+import 'houseGrid_view.dart';
 
 class OwnerProfile extends StatefulWidget {
   const OwnerProfile({ Key? key }) : super(key: key);
@@ -13,6 +14,8 @@ class OwnerProfile extends StatefulWidget {
 }
 
 class _OwnerProfileState extends State<OwnerProfile> {
+  APIManager apiService = APIManager();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,21 +23,33 @@ class _OwnerProfileState extends State<OwnerProfile> {
       elevation: 0,
       backgroundColor: Colors.white,
       centerTitle: true,
-      iconTheme: IconThemeData(color: Colors.black),
+      iconTheme: const IconThemeData(color: Colors.black),
       leading: IconButton(onPressed: (){Navigator.push(
                       context, MaterialPageRoute(builder: (_) =>  CardView()));}, icon: Icon(Icons.arrow_back) ),
       title: const Text(
         "My Profile",
       style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800, color: Colors.black),),
-    ),  
-          body: ListView(
       
-        children: [
+    ),  
+          body: Container(
+            child: FutureBuilder(
+              future: apiService.getProfile(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) { 
+         
+          if (snapshot.hasData){
+            String firstName = snapshot.data!["first_name"];
+            String lastName = snapshot.data!["last_name"];
+            String email = snapshot.data!["email"];
+            String phone = snapshot.data!["phone"];
+            
+
+            return Column(
+            children: [
         
           const SizedBox(
             height: 15,
           ),
-          CircleAvatar(
+          const CircleAvatar(  
             radius: 60,
             backgroundImage: NetworkImage('https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80')
           ),
@@ -43,10 +58,10 @@ class _OwnerProfileState extends State<OwnerProfile> {
 
           ),
           Center(
-            child: Text('Joe Asare', style:TextStyle(fontSize:19,fontWeight: FontWeight.w800, color: Colors.black),
+            child: Text(firstName + ' ' + lastName, style:const TextStyle(fontSize:19,fontWeight: FontWeight.w800, color: Colors.black),
         ),
           ),
-          Center(
+          const Center(
             child: Text('Tarkwa Ghana', style:TextStyle(fontSize:17,fontWeight: FontWeight.w500, color: Colors.black),
         ),
         ),
@@ -56,24 +71,24 @@ class _OwnerProfileState extends State<OwnerProfile> {
           children: [
            
             ListTile(
-              title: Text('Mobile', style:TextStyle(fontSize:19,fontWeight: FontWeight.w800, color: Colors.black),
+              title: const Text('Mobile', style:TextStyle(fontSize:19,fontWeight: FontWeight.w800, color: Colors.black),
         ),
-        subtitle: Text('+233245313051', style:TextStyle(fontSize:16,fontWeight: FontWeight.w500, color: Colors.grey),
+        subtitle: Text(phone, style:const TextStyle(fontSize:16,fontWeight: FontWeight.w500, color: Colors.grey),
         ),
         ),
            ListTile(
-              title: Text('Email', style:TextStyle(fontSize:19,fontWeight: FontWeight.w800, color: Colors.black),
+              title: const Text('Email', style:TextStyle(fontSize:19,fontWeight: FontWeight.w800, color: Colors.black),
         ),
-        subtitle: Text('joeasare@gmail.com', style:TextStyle(fontSize:16,fontWeight: FontWeight.w500, color: Colors.grey),
+        subtitle: Text(email, style:const TextStyle(fontSize:16,fontWeight: FontWeight.w500, color: Colors.grey),
         ),
       ),
           ]
         ), 
           ),
- SizedBox(
+ const SizedBox(
               height: 40,
            ),
-           Container(
+             Container(
               height: 35,
               width: 80,
               decoration: BoxDecoration(
@@ -89,7 +104,7 @@ class _OwnerProfileState extends State<OwnerProfile> {
                 ),
               ),
             ),
-     SizedBox(
+     const SizedBox(
               height: 40,
            ),
 
@@ -100,16 +115,26 @@ class _OwnerProfileState extends State<OwnerProfile> {
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
               child: FlatButton(
                 onPressed: () {
-                  //  Navigator.push(
-                      //  context, MaterialPageRoute(builder: (_) => GridView1()));
+                  // Navigator.push(
+                  //     context, MaterialPageRoute(builder: (_) => LandlordRegisterView()));
                 },
-                child: Text(
+                child: const Text(
                   'Upload Property',
                   style: TextStyle(color: Colors.white, fontSize: 15),
                 ),
               ),
             ),
-            ],
+            ]
+          );
+
+          }else{
+            return const Center(
+                child: Text('No data found'),
+              );
+          }
+          
+        }),
+        
        ),
    );
   }
